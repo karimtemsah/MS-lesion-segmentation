be4da1244f6087bda54e700573b51ef6ae83cf20
Enes Şenel <enessenel@Eness-MacBook-Air.local>
2017-11-14 14:35:11 +0100

    Updated SDNet layers

--- SkipDeconvNet.py
+++ SkipDeconvNet.py
@@ -40,23 +40,9 @@ def create_unpool_layer(input, index, ksize=[1, 2, 2, 1]):
 	output = tf.scatter_nd(ind, uppool, shape=flat_output_shape)
 	return tf.reshape(output, output_shape)
 
-def create_unpool_layer(input, index, ksize=[1, 2, 2, 1]):
-	input_shape = input.get_shape().as_list()
-	output_shape = (input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3])
-
-	flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
-
-	uppool = tf.reshape(input, [np.prod(input_shape)])
-	batch_range = tf.reshape(tf.range(output_shape[0], dtype=index.dtype), shape=[input_shape[0], 1, 1, 1])
-	b = tf.ones_like(index) * batch_range
-	b = tf.reshape(b, [np.prod(input_shape), 1])
-	ind = tf.reshape(index, [np.prod(input_shape), 1])
-	ind = tf.concat([b, ind], 1)
-
-	output = tf.scatter_nd(ind, uppool, shape=flat_output_shape)
-	return tf.reshape(output, output_shape)
-
 def model(input, scope="SkipDeconvNet", reuse=True):
+
+	N = 5
 
 	with tf.name_scope(scope):
 
@@ -74,8 +60,22 @@ def model(input, scope="SkipDeconvNet", reuse=True):
 	    unpool_1 = create_unpool_layer(conv_4, max_pool_3_index)
 	    unpool_1 = tf.concat([unpool_1, conv_3], axis=3)
 
-	    deconv = create_conv_layer(unpool_1, create_weights(7,128,64), create_bias(64))
-	    print(deconv.shape)
+	    deconv_1 = create_conv_layer(unpool_1, create_weights(7,128,64), create_bias(64))
+
+	    unpool_2 = create_unpool_layer(deconv_1, max_pool_2_index)
+	    unpool_2 = tf.concat([unpool_2, conv_2], axis=3)
+
+	    deconv_2 = create_conv_layer(unpool_2, create_weights(7, 128, 64), create_bias(64))
+
+	    unpool_3 = create_unpool_layer(deconv_2, max_pool_1_index)
+	    unpool_3 = tf.concat([unpool_3, conv_1], axis=3)
+
+	    deconv_3 = create_conv_layer(unpool_3, create_weights(7, 128, 64), create_bias(64))
+
+	    #classifier layer
+	    classifier_layer = create_conv_layer(deconv_3, create_weights(1, 64, N), create_bias(N))
+	    softmax = tf.nn.softmax(classifier_layer)
+	    print(softmax.shape)
 
 x = tf.placeholder(tf.float32, shape=(1, 256, 256, 1))
 
