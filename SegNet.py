b038e53a8d09343d028a74f9fb69e682ac978251
Karim Temsah <>
2017-12-30 19:08:53 +0200

    first run

--- SegNet.py
+++ SegNet.py
@@ -13,7 +13,6 @@ from model.inference import model
 from model.evaluation import loss_calc, evaluation
 from model.helpers import *
 
-
 dataset_options = MSSEG2008.Options()
 dataset_options.debug = True
 dataset_options.dir = '/media/data/MSSEG2008/'
@@ -21,11 +20,11 @@ dataset_options.useCrops = True
 dataset_options.cropType = 'random'
 dataset_options.cropWidth = 128
 dataset_options.cropHeight = 128
-dataset_options.numRandomCropsPerSlice = 10  # Not needed when doing center crops
+dataset_options.numRandomCropsPerSlice = 20 # Not needed when doing center crops
 dataset_options.onlyPatchesWithLesions = True
-dataset_options.rotations = range(-5,5)
-dataset_options.partition = {'TRAIN': 0.7, 'VAL': 0.3}
-dataset_options.sliceResolution = None
+dataset_options.rotations = range(-2,2)
+dataset_options.partition = {'TRAIN': 0.6, 'VAL': 0.4}
+dataset_options.sliceResolution = None # We do not need that since we extract crops!
 dataset_options.cache = True
 dataset_options.numSamples = -1
 dataset_options.addInstanceNoise = False
@@ -81,16 +80,18 @@ for e in range(config['numEpochs']):
         batch_data = batch_data.reshape((batch_data.shape[0], 128, 128, 1))
         fetches = {
             'optimizer': train_step,
-            'loss': cross_entropy
+            'loss': cross_entropy,
+            'summary': summary
         }
         results = sess.run(fetches, feed_dict={inputs['data']: batch_data, inputs['labels']: batch_labels})
         avg_loss_in_current_epoch += results['loss']
-        if i % 10 == 0:
-            train_writer.add_summary(results['loss'], i)
+        if i % 100 == 0:
+            #train_writer.add_summary(results['loss'], i)
+            print(i)
     avg_loss_in_current_epoch = avg_loss_in_current_epoch / numTrainSamples
     print("Train: ", avg_loss_in_current_epoch)
     curves['training'] += [avg_loss_in_current_epoch]
-    train_writer.add_summary(avg_loss_in_current_epoch, e)
+    train_writer.add_summary(results['summary'], e)
     if e % 5 == 0:
         saver.save(sess, 'segnet_train_model', global_step=5)
     accumulated_predictions = np.array([])
@@ -103,10 +104,10 @@ for e in range(config['numEpochs']):
         print(batch_labels.shape)
         fetches = {
             'loss': cross_entropy,
-            'accuracy': accuracy_operation
+            'accuracy': accuracy_operation,
+            'summary': summary
         }
-        if i == numValSamples-1:
-            fetches['summary'] = summary
+
         results = sess.run(fetches, feed_dict={inputs['data']: batch_data, inputs['labels']: batch_labels})
         avg_loss_in_current_epoch += results['loss']
         if i == 0:
--- example.py
+++ example.py
@@ -1,8 +1,3 @@
-# Copyright 2017 Christoph Baur <c.baur@tum.de>
-# ==============================================================================
-
-"""Example script on how to use the MSSEG2008 dataset class"""
-
 
 # Basic imports
 import sys
@@ -23,13 +18,13 @@ dataset_options.useCrops = True
 dataset_options.cropType = 'random'
 dataset_options.cropWidth = 128
 dataset_options.cropHeight = 128
-dataset_options.numRandomCropsPerSlice = 10 # Not needed when doing center crops
+dataset_options.numRandomCropsPerSlice = 20 # Not needed when doing center crops
 dataset_options.onlyPatchesWithLesions = True
-dataset_options.rotations = range(-5,5)
+dataset_options.rotations = range(-2,2)
 dataset_options.partition = {'TRAIN': 0.6, 'VAL': 0.4}
-dataset_options.sliceResolution = None
+dataset_options.sliceResolution = None # We do not need that since we extract crops!
 dataset_options.cache = True
-dataset_options.numSamples = 10
+dataset_options.numSamples = -1
 dataset_options.addInstanceNoise = False
 dataset_options.axis = 'axial'
 dataset_options.filterProtocol = ['FLAIR']
@@ -37,4 +32,47 @@ dataset_options.normalizationMethod = 'standardization'
 
 # Center Crops of healthy control images: training, validation and testing patients
 dataset = MSSEG2008(dataset_options)
-dataset.visualize()
+#dataset.visualize()
+
+# Testing next_batch function for train & val
+batchsize = 40
+numEpochs = 5
+for epoch in xrange(numEpochs):
+    numBatches = dataset.numBatches(batchsize, set='TRAIN')
+    for idx in xrange(0, numBatches):
+        batch_real, _, _ = dataset.next_batch(batchsize, set='TRAIN')
+    print('Done with training epoch ({} batches)'.format(numBatches))
+
+    numBatches = dataset.numBatches(batchsize, set='VAL')
+    for idx in xrange(0, numBatches):
+        batch_real, _, _ = dataset.next_batch(batchsize, set='VAL')
+    print('Done with validation ({} batches)'.format(numBatches))
+
+
+
+# How to use the dataset during validation (or testing)
+indices_of_testing_patients = dataset.getPatientsinSet(split='VAL')
+for i in indices_of_testing_patients:
+    patient = dataset.getPatient(i)
+
+    nrrd = NRRD(patient[dataset_options.filterProtocol[0]])
+    nrrd.printStats()
+    #nrrd.visualize(axis=dataset_options.axis)
+
+    nrrd_groundtruth = NRRD(patient['groundtruth'])
+    nrrd_groundtruth.printStats()
+    #nrrd_groundtruth.visualize(axis=dataset_options.axis)
+
+    # In-place normalize the loaded volume
+    nrrd.normalize(method=dataset_options.normalizationMethod)
+
+    # Now go slice by slice over the entire volume and segment every slice.
+    nrrd_prediction = np.zeros(nrrd_groundtruth.shape())
+    for i in range(nrrd.numSlicesAlongAxis(dataset_options.axis)):
+        _slice = nrrd.getSlice(i, dataset_options.axis)
+        #prediction = PREDICT USING YOUR MODEL
+        #nrrd_prediction[:,:,i] = prediction
+
+    # Threshold the volume
+    # Compute metrics between predicted volume and groundtruth
+    # Save prediction as a NRRD file again.
